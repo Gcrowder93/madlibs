f249e724886a26f556090501f8e6442b23f61e49
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-08-27 15:51:40 -0700

    added const

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 // import functions
+const paragraph = document.getElementById('paragraph');
 
+const button = document.getElementById('button');
 
+const verb1 = document.getElementById('verb-1-input');
 
-// reference needed DOM elements
+const noun1 = document.getElementById('noun-1-input');
 
+const verb2 = document.getElementById('verb-2-input');
 
+const adjective1 = document.getElementById('adjective-1-input');
 
+const bodypart1 = document.getElementById('bodypart-1-input');
 
-// set event listeners 
-    // get user input(s)
-    // do any needed work with the value(s)
-    // update DOM to reflect new value(s)
+const name1 = document.getElementById('name-1-input');
+
+const verb3 = document.getElementById('verb-3-input');
+
+const animal1 = document.getElementById('animal-1-input');
+
+const bodypart2 = document.getElementById('bodypart-2-input');
+
+const adjective2 = document.getElementById('adjective-2-input');
+
+const animal2 = document.getElementById('animal-2-input');
